267d6e74ebe823eb5589ca825d237077c2e3dd25
Nikita Klimuk <nikitaklimukwot@gmail.com>
2023-04-13 23:22:12 +0200

    create basic folder structure, create variable.scss file

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
 
 function App() {
+  const apiUrl = "https://images-api.nasa.gov/search?q=space";
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      return data.collection.items;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
